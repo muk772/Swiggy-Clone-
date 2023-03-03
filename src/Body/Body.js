b744f82c442198e3ba9afeb6aee420fd3f010157
Mukund Madhav <Mukund.Madhav@in.niit.com>
2023-03-04 00:42:22 +0530

    fetch call added

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 const Body = () => {
+  const [resturantsList, setResturantsList] = useState([]);
   useEffect(() => {
     resturantsData();
   }, []);
 
-  const resturantsData = () => {
-    fetch(
+ 
+  async function resturantsData() {
+    const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.894621997950024&lng=77.61992063373327&page_type=DESKTOP_WEB_LISTING"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-  };
+    );
+    const json = await data.json();
+    console.log("Json---", json);
+    setResturantsList(json?.data?.cards[2]?.data?.data?.cards);
+    console.log("resturants inside console", resturantsList);
+  }
 
+  console.log("resturants", resturantsList);
+  
   return <div>Body</div>;
 };
 
